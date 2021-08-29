932321a70deb934eb1bbb334586837dfe34b3dff
nicksonKLEY <nicksonkley14@gmail.com>
2021-08-28 21:49:45 -0400

    initial commit

---
+++ ELD_Challenge/ELD_Challenge/ELD_Challenge.h
@@ -0,0 +1,18 @@
+//
+//  ELD_Challenge.h
+//  ELD_Challenge
+//
+//  Created by Nickson Kley Gonçalves Da Silva on 28/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ELD_Challenge.
+FOUNDATION_EXPORT double ELD_ChallengeVersionNumber;
+
+//! Project version string for ELD_Challenge.
+FOUNDATION_EXPORT const unsigned char ELD_ChallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ELD_Challenge/PublicHeader.h>
+
+
